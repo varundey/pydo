78e58ac68b3fb688eb35e790031bb4f720118b6a
Varun Dey <varun.silversurfer@gmail.com>
2016-08-14 05:54:20 +0530

    Updated and patched!

---
+++ messages.py
@@ -0,0 +1,21 @@
+success={
+	"added":" successfully added in your to-do list!",	
+	"done":" successfully marked as done!",
+
+}
+
+error={
+	"done":"Looks like your to-do list is empty. Try adding some items before doing this operation.",
+	"view":"No to-do list found. Create one with 'to-do'",
+	"throw":"You do not have any to-do list to throw",
+	"empty_task":"C'mon! Don't be that lazy...",
+	"task_exists1":"Uh oh! Looks like the id you entered is assigned for ",
+	"task_exists1":". Try giving a unique id.",
+	"wrong":"That didn't work! Try entering in following format:\n1, water the plants",
+	"args":"Confused? Worry not. Check 'todo --man'"	
+}
+
+info={
+	"view":"Your to-do list contains following tasks:",
+	"throw":"Preparing to fold your list and throw it in bin.....",
+}
